ee74c5fea6012ea57e28c04da319c414dab53dc9
Soumyajit Kolay <skolay@vivaldi.net>
2024-07-15 17:14:32 +0530

    Added REST POST API, Minor improvements

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, jsonify
 import pickle
 import numpy as np
+import pandas as pd
 
+loaded_model = pickle.load(open("model.pkl", "rb"))
 app = Flask(__name__)
 
 
@@ -11,10 +13,18 @@ def index():
 
 def ValuePredictor(to_predict_list):
     to_predict = np.array(to_predict_list).reshape(1, 12)
-    loaded_model = pickle.load(open("model.pkl", "rb"))
     result = loaded_model.predict(to_predict)
     return result[0]
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    json_ = request.json
+    data = json_[0]
+    output = []
+    for i in range(len(json_)):
+        prediction = loaded_model.predict(np.array(list(json_[i].values())).reshape(1, 12))
+        output.append(int((int(prediction[0]) == 1)))
+    return jsonify(Predictions = output, status = 200, mimetype = 'application/json')
 
 @app.route("/result", methods=["POST"])
 def result():
